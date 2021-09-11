4f890bc3c2a8f732b6f95fab4f2416c800ab668a
cubercsl <2014cais01@gmail.com>
2021-09-11 22:51:44 +0800

    feat: deno codeforces rating

---
+++ api/codeforces.ts
@@ -0,0 +1,152 @@
+#!/usr/bin/env deno run --allow-net
+
+import { ExtendedRequest, ky, ServerRequest } from "../deps.ts";
+
+const CF_API = "https://codeforces.com/api/user.info";
+const SHIELD_API = "https://img.shields.io/badge";
+
+interface Response {
+  status: string;
+  comment: string;
+  result: User[];
+}
+
+// http://codeforces.com/apiHelp/objects#User
+interface User {
+  handle: string;
+  email: string;
+  vkId: string;
+  openId: string;
+  firstName: string;
+  lastName: string;
+  country: string;
+  city: string;
+  organization: string;
+  contribution: number;
+  rank: string;
+  rating: number;
+  maxRank: string;
+  maxRating: number;
+  lastOnlineTimeSeconds: number;
+  registrationTimeSeconds: number;
+  friendOfCount: number;
+  avatar: string;
+  titlePhoto: string;
+}
+
+export interface getImageOption {
+  handle: string;
+  rank: string;
+  color: string;
+  rating?: number | undefined;
+}
+
+const ratingColors = new Map([
+  ["legendary grandmaster", "ff0000"],
+  ["international grandmaster", "ff0000"],
+  ["grandmaster", "ff0000"],
+  ["international master", "ff8c00"],
+  ["master", "ff8c00"],
+  ["candidate master", "aa00aa"],
+  ["expert", "0000ff"],
+  ["specialist", "03a89e"],
+  ["pupil", "008000"],
+  ["newbie", "808080"],
+  ["unrated", "000000"],
+]);
+
+export async function getImage({
+  handle,
+  rank,
+  color,
+  rating = undefined,
+  ...params
+}: getImageOption): Promise<string> {
+  const toTitleCase = (str: string) =>
+    str.replace(/\b\S/g, (t) => t.toUpperCase());
+  const escapedHandle = handle.replace("_", "__").replace("-", "--");
+  const ratingStr = rating
+    ? `${toTitleCase(rank)} ${rating}`
+    : `${toTitleCase(rank)}`;
+  const svg = await ky
+    .get(`${SHIELD_API}/${escapedHandle}-${ratingStr}-${color}.svg`, {
+      searchParams: {
+        cacheSeconds: 86400,
+        logo: "Codeforces",
+        ...params,
+      },
+    }).text();
+  return svg;
+}
+
+export async function getUserData(handle: string): Promise<Response> {
+  const data: Response = await ky
+    .get(`${CF_API}`, {
+      searchParams: {
+        handles: handle,
+      },
+    }).json();
+  return data;
+}
+
+if (import.meta.main) {
+  if (!Deno.args[0]) {
+    Deno.exit(1);
+  }
+  const respose = await getUserData(Deno.args[0]);
+  console.log(respose);
+}
+
+export default async (req: ServerRequest) => {
+  const { search } = new ExtendedRequest(req);
+  const { user, ...params } = search;
+  if (!user) {
+    req.respond({
+      status: 404,
+      body: await getImage({
+        handle: "404",
+        rank: "user not found",
+        color: "critical",
+        ...params,
+      }),
+      headers: new Headers({
+        "content-type": "image/svg+xml",
+      }),
+    });
+    return
+  }
+  const respose = await getUserData(user);
+  if (respose.status != "OK") {
+    req.respond({
+      status: 404,
+      body: await getImage({
+        handle: "404",
+        rank: "user not found",
+        color: "critical",
+        ...params,
+      }),
+      headers: new Headers({
+        "content-type": "image/svg+xml",
+      }),
+    });
+  } else {
+    const handle = respose.result[0].handle;
+    const rating = respose.result[0].rating;
+    const rank = respose.result[0].rank ? respose.result[0].rank : "unrated";
+    const color = ratingColors.get(rank);
+    req.respond({
+      status: 200,
+      body: await getImage({
+        handle: handle,
+        rating: rating,
+        rank: rank,
+        color: String(color),
+        ...params,
+      }),
+      headers: new Headers({
+        "cache-control": "s-maxage=86400",
+        "content-type": "image/svg+xml",
+      }),
+    });
+  }
+};
---
+++ deps.ts
@@ -0,0 +1,5 @@
+// @deno-types="https://cdn.jsdelivr.net/npm/ky@0.27.0/index.d.ts"
+export { default as ky } from 'https://cdn.jsdelivr.net/npm/ky@0.27.0/index.js'
+
+export * from 'https://deno.land/std@0.79.0/http/server.ts'
+export * from 'https://deno.land/x/deno_extras@v0.4.3/mod.ts'
